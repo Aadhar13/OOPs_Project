0401c360efab607071917338f025bc25c9472380
Aadhar13 <aadhar.dhote321@gmail.com>
2025-03-01 01:57:13 +0530

    first commit

---
+++ src/com/bank/Doctor.java
@@ -0,0 +1,55 @@
+package com.bank;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Doctor {
+private Connection connection;
+
+public Doctor(Connection connection) {
+	this.connection=connection;
+}
+
+public void viewDoctors() {
+	String query="select * from doctors";
+	try {
+		PreparedStatement preparedStatement=connection.prepareStatement(query);
+		ResultSet resultSet=preparedStatement.executeQuery();
+		 System.out.println("Doctors: ");
+         System.out.println("+------------+--------------------+------------------+");
+         System.out.println("| Doctor Id  | Name               | Specialization   |");
+         System.out.println("+------------+--------------------+------------------+");
+		while(resultSet.next()) {
+			System.out.println(resultSet.getInt("id")+"           |"+resultSet.getString("name")+"          |"+resultSet.getString("specialization"));
+			 System.out.println("+------------+--------------------+------------------+");
+        }
+		
+	}catch(SQLException e) {
+		e.printStackTrace();
+	}
+}
+
+public boolean getDoctorById(int id){
+	String query="SELECT * FROM doctors WHERE id=?";
+try {
+	PreparedStatement preparedStatement=connection.prepareStatement(query);
+	preparedStatement.setInt(1,id);
+	ResultSet resultSet=preparedStatement.executeQuery();
+	if(resultSet.next()) {
+		return true;
+	}else {
+		return false;
+	}
+}catch (SQLException e){
+    e.printStackTrace();
+}
+return false;
+
+
+
+}
+
+
+}
